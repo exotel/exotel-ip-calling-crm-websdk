045f6083042067cf36e868637db01b75c7f67bf4
Abhishek <abhishek@exotel.com>
2024-06-12 10:01:38 +0530

    impl CRMWebSDK and WebPhoneSDK

---
+++ src/CRMWebSDK.ts
@@ -0,0 +1,130 @@
+import { icoreBaseURL, voipDomainSIP, voipDomain } from "./Constants";
+import { User } from "./User";
+import { SIPAccountInfo } from "./SipAccountInfo";
+import { WebPhoneSDK } from "./WebPhoneSDK";
+
+// Fetches account details, user details, and their settings
+class CRMWebSDK {
+  #accessToken: string;
+  #agentUserID: string;
+  #autoConnectVOIP: boolean;
+
+  constructor(accesssToken: string, agentUserID: string, autoConnectVOIP: boolean = false) {
+    if (!accesssToken) {
+      console.error("empty access token passed");
+      return;
+    }
+    if (!agentUserID) {
+      console.error("empty agentUserID passed");
+      return;
+    }
+    this.#accessToken = accesssToken;
+    this.#agentUserID = agentUserID;
+    this.#autoConnectVOIP = autoConnectVOIP;
+  }
+
+  #app: { ExotelAccountSid: string };
+  #appSettings: any;
+  #userData: User;
+
+  // Initialises CRMWebSDK, Phone Object and registers callbacks
+  async Initialize(
+    sofPhoneListenerCallback,
+    softPhoneRegisterEventCallBack = null,
+    softPhoneSessionCallback = null
+  ): Promise<WebPhoneSDK | void> {
+    await this.#loadSettings();
+    const sipInfo = this.#getSIPInfo();
+    if (!sipInfo) {
+      return;
+    }
+
+    const webPhone = new WebPhoneSDK(this.#accessToken, this.#userData)
+    return webPhone.Initialize(
+      sipInfo,
+      sofPhoneListenerCallback,
+      this.#autoConnectVOIP,
+      softPhoneRegisterEventCallBack,
+      softPhoneSessionCallback
+    );
+  }
+
+  async #loadSettings() {
+    // Load app
+    try {
+      const response = await fetch(`${icoreBaseURL}/v2/integrations/app`, {
+        method: "GET",
+        headers: { Authorization: this.#accessToken },
+      });
+
+      const appResponse = await response.json();
+      this.#app = appResponse.Data;
+    } catch (error) {
+      console.error("error loading app:", error);
+    }
+
+    /**
+     * TODO: Right now app settings response returns preference related to UI widget
+     * location, which doesn't exist yet for this CRMWebSDK.
+     * Now that we have separated the UI widget and the webSDK, we need to make fetching app settings optional
+     * ie make this request only when the UI widget is initialised
+     */
+
+    // Load app settings for the tenant
+    try {
+      const settingsResponse = await fetch(
+        `${icoreBaseURL}/v2/integrations/app_setting`,
+        {
+          method: "GET",
+          headers: { Authorization: this.#accessToken },
+        }
+      );
+      this.#appSettings = await settingsResponse.json();
+    } catch (error) {
+      console.error("error loading app settings:", error);
+    }
+
+    // Load user mapping for the tenant
+    try {
+      const response = await fetch(
+        `${icoreBaseURL}/v2/integrations/usermapping?user_id=${this.#agentUserID}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: this.#accessToken,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const userMappingResponse = await response.json();
+      this.#userData = new User(userMappingResponse.Data);
+    } catch (error) {
+      console.error("error loading user details:", error);
+    }
+  }
+
+  #getSIPInfo(): SIPAccountInfo | void {
+    if (!this.#userData) {
+      console.error("userData must be configured to get sip info");
+      return;
+    }
+    if (!this.#app) {
+      console.error("app must be configured to get sip info");
+      return;
+    }
+    const sipAccountInfo: SIPAccountInfo = {
+      userName: this.#userData.sipId.split(":")[1], // sipInfo.Username,
+      authUser: this.#userData.sipId.split(":")[1], //sipInfo.Username,
+      sipdomain: this.#app.ExotelAccountSid + "." + voipDomainSIP, //sipInfo.Domain,
+      domain: voipDomain + ":443", // sipInfo.HostServer + ":" + sipInfo.Port,
+      displayname: this.#userData.exotelUserName, //sipInfo.DisplayName,
+      secret: this.#userData.sipSecret, //sipInfo.Password,
+      port: "443", //sipInfo.Port,
+      security: "wss", //sipInfo.Security,
+      endpoint: "wss", //sipInfo.EndPoint
+    };
+    return sipAccountInfo;
+  }
+}
+
+export default CRMWebSDK;
---
+++ src/WebPhoneSDK.ts
@@ -0,0 +1,164 @@
+import { ExotelWebClient } from "@exotel-npm-dev/webrtc-client-sdk";
+import { Call } from "@exotel-npm-dev/webrtc-client-sdk/src/api/callAPI/Call";
+import { User } from "./User";
+import { icoreBaseURL } from "./Constants";
+import { SIPAccountInfo } from "./SipAccountInfo";
+
+
+interface MakeCallCallback {
+  (status: "success" | "failed", data: any): void;
+}
+
+interface CallEventData {
+  callFromNumber: string;
+  status: string;
+  // Add other properties as needed
+}
+
+interface CallListenerCallback {
+  (event: string, callData: CallEventData): void;
+}
+
+interface RegisterEventCallback {
+  (event: string, callData: CallEventData): void;
+}
+
+interface SessionCallback {
+  (event: string, callData: CallEventData): void;
+}
+
+export class WebPhoneSDK {
+  #accessToken: string;
+  #user: User;
+
+  //this constructor is invoked when called from ippstn.js,
+  constructor(accessToken: string, user: User) {
+    this.#accessToken = accessToken; // This access token is understood by icore which makes this SDK dependent on it
+    this.#user = user;
+  }
+
+  #softPhoneRegisterEventCallBack: any;
+  #softPhoneCallListenerCallback: CallListenerCallback;
+  #exWebClient: ExotelWebClient;
+  #sipInfo: SIPAccountInfo;
+  #softPhoneSessionCallback: any;
+
+  Initialize(
+    sipInfo: SIPAccountInfo,
+    softPhoneCallListenerCallback: CallListenerCallback,
+    autoConnectVOIP = false,
+    softPhoneRegisterEventCallBack,
+    softPhoneSessionCallback
+  ) {
+    this.#sipInfo = sipInfo;
+    this.#softPhoneCallListenerCallback = softPhoneCallListenerCallback;
+    this.#softPhoneRegisterEventCallBack = softPhoneRegisterEventCallBack;
+    this.#softPhoneSessionCallback = softPhoneSessionCallback;
+
+    this.#exWebClient = new ExotelWebClient();
+    this.#exWebClient.initWebrtc(
+      sipInfo,
+      this.#registerEventCallBack,
+      this.#callListenerCallback,
+      this.#sessionCallback
+    );
+
+    if (autoConnectVOIP) {
+      this.RegisterDevice();
+    }
+  }
+
+  RegisterDevice() {
+    this.#exWebClient.DoRegister();
+  }
+
+  UnRegisterDevice() {
+    this.#exWebClient.unregister(this.#sipInfo);
+  }
+
+  #call: Call;
+
+  /**
+   * _callListenerCallback is a wrapper over the listener callback
+   * provided at the time of initialisation to allow us to log stuff
+   * @param callObj
+   * @param eventType
+   * @param sipInfo
+   */
+  #callListenerCallback(callObj, eventType, sipInfo: SIPAccountInfo) {
+    // let call = this._exWebClient.getCall();
+    this.#call = this.#exWebClient.getCall();
+    callObj.callFromNumber = this.#exWebClient.callFromNumber;
+    console.info(this.#call?.callDetails());
+    this.#softPhoneCallListenerCallback(eventType, callObj);
+  }
+
+  #registerEventCallBack(state, sipInfo: SIPAccountInfo) {
+    this.#softPhoneRegisterEventCallBack(state);
+  }
+
+  #sessionCallback(state, sipInfo: SIPAccountInfo) {
+    console.info("Session state:", state, "for number...", sipInfo);
+    this.#softPhoneSessionCallback(state, sipInfo);
+  }
+
+  AcceptCall() {
+    this.#call?.Answer();
+  }
+
+  RejectCall() {
+    this.#call?.Hangup();
+  }
+
+  async MakeCall(number: string, callback: MakeCallCallback) {
+    const payload = {
+      customer_id: this.#user.customerId,
+      app_id: this.#user.appId,
+      to: number,
+      user_id: this.#user.appUserId,
+    };
+
+    const headers = {
+      Authorization: this.#accessToken,
+      "Content-Type": "application/json",
+    };
+
+    /**
+     * We are calling icore here to place a call, which makes this
+     * SDK dependent on icore unfortunately
+     */
+    try {
+      const response = await fetch(
+        icoreBaseURL + "/v2/integrations/call/outbound_call",
+        {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(payload),
+        }
+      );
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error("error making call:", response.statusText, errorText);
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      console.info("successfully placed call:", data);
+      callback("success", data);
+    } catch (error) {
+      console.error("Error:", error);
+      callback("failed", error);
+    }
+  }
+
+  ToggleHoldButton() {
+    this.#call?.HoldToggle();
+    if (this.#softPhoneCallListenerCallback) {
+      this.#softPhoneCallListenerCallback("holdtoggle", this.#call);
+    }
+  }
+
+  ToggleMuteButton() {
+    this.#call.Mute();
+    this.#softPhoneCallListenerCallback("mutetoggle", this.#call);
+  }
+}
